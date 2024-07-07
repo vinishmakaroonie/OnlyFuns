faa0e2713bd5506006a84f036b0b7fc71698cf14
vinishmakaroonie <138175501+vinishmakaroonie@users.noreply.github.com>
2024-07-07 14:02:19 +0800

    Update cancel_reservation.php

--- cancel_reservation.php
+++ cancel_reservation.php
@@ -18,7 +18,7 @@ if (isset($_GET['id'])) {
     foreach ($reservations as $index => $reservation) {
         if ($reservation['id'] === $reservationId) {
             // Calculate penalty based on cancellation date
-            $reservationDate = strtotime($reservation['reservation_date']);
+            $reservationDate = strtotime($reservation['check_in_date']);
             $currentDate = time();
             $daysDifference = ($reservationDate - $currentDate) / (60 * 60 * 24);
 
@@ -48,18 +48,27 @@ if (isset($_GET['id'])) {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Cancel Reservation</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1> OnlyFuns Hotel Reservation </h1>
+    <div>
+        <a href="index.php">Home</a>
+        <a href="view_rooms.php">View Rooms</a>
+        <a href="make_reservation.php">Make Reservation</a>
+        <a href="view_reservations.php">View Reservation</a>
+        <a href="cancel_reservation.php">Cancel Reservation</a>
+        <a href="change_room.php">Change Room</a>
+    </div>
     <div class="container">
         <h1>Cancel Reservation</h1>
         <p><?php echo $message; ?></p>
-        <a href="view_reservations.php">Back to Reservations</a>
-        <a href="index.php">Home</a>
+        <a href="view_reservations.php" class="button">Back to Reservations</a>
+        <a href="index.php" class="button">Home</a>
     </div>
 </body>
 </html>
