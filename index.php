239d25d2a03612676df586e5ccd708417d6f3bd2
vinishmakaroonie <138175501+vinishmakaroonie@users.noreply.github.com>
2024-07-10 17:20:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
         <div class="navigation">
             <a href="index.php">Home</a>
             <a href="view_rooms.php">View Rooms</a>
-            <a href="make_reservation.php">Make Reservation</a>
             <a href="view_reservations.php">View Reservation</a>
             <a href="cancel_reservation.php">Cancel Reservation</a>
             <a href="change_room.php">Change Room</a>
